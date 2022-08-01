a847e084757e0ec75761bdb3574fec598eb14780
nushypaenisa <Nashy.nisa@gmail.com>
2022-08-01 22:33:31 +0300

    Update ContentView.swift

--- weather_app/ContentView.swift
+++ weather_app/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomLeading)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+//                    .background(Color.red)
+//                    .frame(width: 200, height: 200)
+                Spacer()
+            }
+           
+        }
     }
 }
 
