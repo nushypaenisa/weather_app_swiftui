4e3dd4208308934739c1bc97fb410de883ee1327
Nash <nashy.nisa@gmail.com>
2022-08-01 21:54:56 +0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Martha Nashipae on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
